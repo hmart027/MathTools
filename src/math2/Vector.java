40474cb54fbfab926ce9e29d5d8bae070d6973d4
Harold Martin <harold.martin92@gmail.com>
2017-11-16 00:36:06 -0500

    Vector class changes.

--- src/math2/Vector.java
+++ src/math2/Vector.java
@@ -10,6 +10,14 @@ public class Vector {
 		return out;
 	}
 	
+	public static double[] add(double[] a1, double[] a2){
+		if(a1.length!=a2.length) return null;
+		double[] out = new double[a1.length];
+		for(int i=0; i<a1.length; i++)
+			out[i] = a1[i]+a2[i];
+		return out;
+	}
+	
 	public static float[] substract(float[] a1, float[] a2){
 		if(a1.length!=a2.length) return null;
 		float[] out = new float[a1.length];
@@ -18,9 +26,25 @@ public class Vector {
 		return out;
 	}
 	
+	public static double[] substract(double[] a1, double[] a2){
+		if(a1.length!=a2.length) return null;
+		double[] out = new double[a1.length];
+		for(int i=0; i<a1.length; i++)
+			out[i] = a1[i]-a2[i];
+		return out;
+	}
+	
 	public static float[] scalarMult(float[] array, float scalar){
 		if(array==null || array.length==0) return null;
 		float[] out = new float[array.length];
+		for(int i=0; i<array.length; i++)
+			out[i] = array[i]*scalar;
+		return out;
+	}
+	
+	public static double[] scalarMult(double[] array, double scalar){
+		if(array==null || array.length==0) return null;
+		double[] out = new double[array.length];
 		for(int i=0; i<array.length; i++)
 			out[i] = array[i]*scalar;
 		return out;
